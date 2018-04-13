64d783a73af79497d15c567c3b0715b77a392c6e
orfire <edouard.blanchard@gmail.com>
2018-04-13 17:47:18 +0200

    Ajout boutton quitter

--- Mercure/Mercure/Fenetre_Selection_XML.Designer.cs
+++ Mercure/Mercure/Fenetre_Selection_XML.Designer.cs
@@ -34,13 +34,15 @@
             this.Bouton_MAJ = new System.Windows.Forms.Button();
             this.Bar_Chargement_XML = new System.Windows.Forms.ProgressBar();
             this.Bouton_Annuler = new System.Windows.Forms.Button();
+            this.Bouton_Quitter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Bouton_Parcourir
             // 
-            this.Bouton_Parcourir.Location = new System.Drawing.Point(260, 24);
+            this.Bouton_Parcourir.Location = new System.Drawing.Point(362, 29);
+            this.Bouton_Parcourir.Margin = new System.Windows.Forms.Padding(4);
             this.Bouton_Parcourir.Name = "Bouton_Parcourir";
-            this.Bouton_Parcourir.Size = new System.Drawing.Size(75, 23);
+            this.Bouton_Parcourir.Size = new System.Drawing.Size(100, 28);
             this.Bouton_Parcourir.TabIndex = 0;
             this.Bouton_Parcourir.Text = "Parcourir";
             this.Bouton_Parcourir.UseVisualStyleBackColor = true;
@@ -48,19 +50,20 @@
             // 
             // Affichage_Chemin_Fichier_XML
             // 
-            this.Affichage_Chemin_Fichier_XML.Location = new System.Drawing.Point(22, 26);
+            this.Affichage_Chemin_Fichier_XML.Location = new System.Drawing.Point(29, 32);
+            this.Affichage_Chemin_Fichier_XML.Margin = new System.Windows.Forms.Padding(4);
             this.Affichage_Chemin_Fichier_XML.Name = "Affichage_Chemin_Fichier_XML";
-            this.Affichage_Chemin_Fichier_XML.Size = new System.Drawing.Size(218, 20);
+            this.Affichage_Chemin_Fichier_XML.Size = new System.Drawing.Size(315, 22);
             this.Affichage_Chemin_Fichier_XML.TabIndex = 3;
             // 
             // Bouton_Integrer
             // 
             this.Bouton_Integrer.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.Bouton_Integrer.Location = new System.Drawing.Point(22, 70);
-            this.Bouton_Integrer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Bouton_Integrer.Location = new System.Drawing.Point(29, 86);
+            this.Bouton_Integrer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Bouton_Integrer.Name = "Bouton_Integrer";
             this.Bouton_Integrer.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Bouton_Integrer.Size = new System.Drawing.Size(75, 23);
+            this.Bouton_Integrer.Size = new System.Drawing.Size(100, 28);
             this.Bouton_Integrer.TabIndex = 1;
             this.Bouton_Integrer.Text = "Integrer";
             this.Bouton_Integrer.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@
             // 
             // Bouton_MAJ
             // 
-            this.Bouton_MAJ.Location = new System.Drawing.Point(142, 70);
+            this.Bouton_MAJ.Location = new System.Drawing.Point(136, 86);
+            this.Bouton_MAJ.Margin = new System.Windows.Forms.Padding(4);
             this.Bouton_MAJ.Name = "Bouton_MAJ";
-            this.Bouton_MAJ.Size = new System.Drawing.Size(75, 23);
+            this.Bouton_MAJ.Size = new System.Drawing.Size(100, 28);
             this.Bouton_MAJ.TabIndex = 2;
             this.Bouton_MAJ.Text = "Mise à jour";
             this.Bouton_MAJ.UseVisualStyleBackColor = true;
@@ -79,29 +83,41 @@
             // Bar_Chargement_XML
             // 
             this.Bar_Chargement_XML.ForeColor = System.Drawing.Color.Lime;
-            this.Bar_Chargement_XML.Location = new System.Drawing.Point(0, 110);
-            this.Bar_Chargement_XML.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Bar_Chargement_XML.Location = new System.Drawing.Point(0, 135);
+            this.Bar_Chargement_XML.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Bar_Chargement_XML.Name = "Bar_Chargement_XML";
-            this.Bar_Chargement_XML.Size = new System.Drawing.Size(358, 19);
+            this.Bar_Chargement_XML.Size = new System.Drawing.Size(477, 23);
             this.Bar_Chargement_XML.TabIndex = 4;
             // 
             // Bouton_Annuler
             // 
             this.Bouton_Annuler.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Bouton_Annuler.Location = new System.Drawing.Point(260, 70);
+            this.Bouton_Annuler.Location = new System.Drawing.Point(244, 86);
+            this.Bouton_Annuler.Margin = new System.Windows.Forms.Padding(4);
             this.Bouton_Annuler.Name = "Bouton_Annuler";
-            this.Bouton_Annuler.Size = new System.Drawing.Size(75, 23);
+            this.Bouton_Annuler.Size = new System.Drawing.Size(100, 28);
             this.Bouton_Annuler.TabIndex = 5;
             this.Bouton_Annuler.Text = "Annuler";
             this.Bouton_Annuler.UseVisualStyleBackColor = true;
             // 
+            // Bouton_Quitter
+            // 
+            this.Bouton_Quitter.Location = new System.Drawing.Point(362, 86);
+            this.Bouton_Quitter.Name = "Bouton_Quitter";
+            this.Bouton_Quitter.Size = new System.Drawing.Size(100, 28);
+            this.Bouton_Quitter.TabIndex = 6;
+            this.Bouton_Quitter.Text = "Quitter";
+            this.Bouton_Quitter.UseVisualStyleBackColor = true;
+            this.Bouton_Quitter.Click += new System.EventHandler(this.BoutonCliquer_Click);
+            // 
             // Fenetre_Selection_XML
             // 
             this.AcceptButton = this.Bouton_Integrer;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.Bouton_Annuler;
-            this.ClientSize = new System.Drawing.Size(356, 129);
+            this.ClientSize = new System.Drawing.Size(475, 159);
+            this.Controls.Add(this.Bouton_Quitter);
             this.Controls.Add(this.Bouton_Annuler);
             this.Controls.Add(this.Bar_Chargement_XML);
             this.Controls.Add(this.Affichage_Chemin_Fichier_XML);
@@ -109,7 +125,7 @@
             this.Controls.Add(this.Bouton_Integrer);
             this.Controls.Add(this.Bouton_Parcourir);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Fenetre_Selection_XML";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Fenetre_Selection_XML";
@@ -127,5 +143,6 @@
         private System.Windows.Forms.Button Bouton_Integrer;
         private System.Windows.Forms.ProgressBar Bar_Chargement_XML;
         private System.Windows.Forms.Button Bouton_Annuler;
+        private System.Windows.Forms.Button Bouton_Quitter;
     }
 }
