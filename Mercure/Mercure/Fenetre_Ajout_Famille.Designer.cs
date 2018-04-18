88106dd909fcb6aad4c86718c48d8a724d6a14ec
orfire <edouard.blanchard@gmail.com>
2018-04-18 15:50:52 +0200

    gougou betises

--- Mercure/Mercure/Fenetre_Ajout_Famille.Designer.cs
+++ Mercure/Mercure/Fenetre_Ajout_Famille.Designer.cs
@@ -28,11 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.olala_la_la = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // olala_la_la
+            // 
+            this.olala_la_la.Location = new System.Drawing.Point(100, 81);
+            this.olala_la_la.Name = "olala_la_la";
+            this.olala_la_la.Size = new System.Drawing.Size(75, 23);
+            this.olala_la_la.TabIndex = 0;
+            this.olala_la_la.Text = "olala la la";
+            this.olala_la_la.UseVisualStyleBackColor = true;
+            this.olala_la_la.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Fenetre_Ajout_Famille
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.olala_la_la);
+            this.Name = "Fenetre_Ajout_Famille";
             this.Text = "Fenetre_Ajout_Famille";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button olala_la_la;
     }
 }
